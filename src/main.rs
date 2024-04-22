1722a47e35881d65226abe5cfe0f79d72d70fa6d
kjellhar <kjell@lentic.net>
2024-04-22 13:29:11 +0200

    Making the code more rusty (I think)

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,45 @@
-use core::num;
-use std::collections::linked_list;
 use std::fs::File;
 use std::io::{self, BufRead};
 use regex::Regex;
 
 #[derive(Debug, Clone)]
 struct Number {
-    pub value: u32,
+    pub value: usize,
     pub is_valid: bool,
-    pub on_line: u32,
-    pub start_on_col: u32,
-    pub end_on_col: u32,
-    pub adjacent_symbols: String,
+    pub on_line: usize,
+    pub start_on_col: usize,
+    pub end_on_col: usize,
 }
 
 impl Number {
-    fn new(value: u32, on_line: u32, start_on_col: u32, end_on_col: u32) -> Number {
+    fn new(value: usize, on_line: usize, start_on_col: usize, end_on_col: usize) -> Number {
         Number {
             value,
             is_valid: false,
             on_line,
             start_on_col,
             end_on_col,
-            adjacent_symbols: String::new(),
+        }
+    }
+
+//    pub fn is_adjacent(&mut self, symb: &Symbol) {
+//        todo!();
+//    }
+}
+
+#[derive(Debug, Clone)]
+struct Symbol {
+    pub symbol: char,
+    pub on_line: usize,
+    pub on_col: usize,
+}
+
+impl Symbol {
+    fn new(symbol: char, on_line: usize, on_col: usize) -> Symbol {
+        Symbol {
+            symbol,
+            on_line,
+            on_col,
         }
     }
 }
@@ -33,48 +50,36 @@ const FILE_NAME: &str = "testdata.txt";
 fn main() {
 
     let mut matched_numbers: Vec<Number> = Vec::new();
+    let mut matched_symbols: Vec<Symbol> = Vec::new();
     let reg_match_number: Regex = Regex::new(r"(\d+)").unwrap();
-    let reg_match_symbol: Regex = Regex::new(r"([^0-9.])").unwrap();
+    let reg_match_symbol: Regex = Regex::new(r"([^0-9.{1}])").unwrap();
 
     // Read each line of file into a vector
     let lines: Vec<String> = read_lines(FILE_NAME); 
 
-    let mut sum  = 0;
-
-    for (line_number, line) in lines.iter().enumerate() {
-        let found_numbers: regex::Matches<'_, '_> = reg_match_number.find_iter(&line);
-        for number in found_numbers {
-            let value: u32 = number.as_str().parse().unwrap();
-            let start_col: u32 = number.start() as u32;
-            let end_col: u32 = (number.end() as u32)-1;
-
-            let mut new_number: Number = Number::new(value, line_number as u32, start_col, end_col);
-
-            // Find adjacent fields
-            let mut adjacent_string = String::new();
-            let line_iter = line_number.saturating_sub(1)..(line_number+1).min(lines.len()-1);
-            let char_iter = (start_col.saturating_sub(1) as usize)..((end_col as usize)+1).min(&line.len()-1);
-            for l in line_iter {
-                adjacent_string.push_str(lines[l].to_string()[char_iter.clone()].to_string().as_str());
-            }
-            
-            let found_symbols: regex::Matches<'_, '_> = reg_match_symbol.find_iter(&line);
-            for symbol in found_symbols {
-                let symbol_char: char = symbol.as_str().chars().next().unwrap();
-                new_number.adjacent_symbols.push(symbol_char);
-            }
-
-            if new_number.adjacent_symbols.is_empty() { 
-                new_number.is_valid = false
-            } else {
-                new_number.is_valid = true;
-                sum += new_number.value;
-            }        
-            matched_numbers.push(new_number.clone());
-        }
+    // Collect all numbers and symbols
+    for (line_number, line) in lines.iter().enumerate() {     
+        matched_numbers.extend(reg_match_number.find_iter(&line)
+            .map(|m| Number::new(m.as_str().parse().unwrap(), 
+                                             line_number, 
+                                             m.start(), 
+                                             m.end()))
+            .collect::<Vec<Number>>().clone());
+        
+        matched_symbols.extend(reg_match_symbol
+            .find_iter(&line)
+            .map(|m| Symbol::new(m.as_str().chars().next().unwrap(),
+                                                line_number,
+                                                m.start()))
+            .collect::<Vec<Symbol>>().clone());      
     }
+
+    println!("{:?}", matched_numbers);
+    println!("{:?}", matched_symbols);
     
-    println!("{}", sum);
+
+
+    
 }
 
 
