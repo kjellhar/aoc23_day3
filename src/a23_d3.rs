e0c287157e35085325d81eee79523d0293b9c96c
kjellhar <kjell@lentic.net>
2024-04-23 14:46:17 +0200

    -

--- src/a23_d3.rs
+++ src/a23_d3.rs
@@ -1,6 +1,6 @@
+use regex::Regex;
 use std::fs::File;
 use std::io::{self, BufRead};
-use regex::Regex;
 
 #[derive(Debug, Clone)]
 pub(crate) struct Number {
@@ -11,7 +11,12 @@ pub(crate) struct Number {
 }
 
 impl Number {
-    pub(crate) fn new(value: usize, on_line: usize, start_on_col: usize, end_on_col: usize) -> Number {
+    pub(crate) fn new(
+        value: usize,
+        on_line: usize,
+        start_on_col: usize,
+        end_on_col: usize,
+    ) -> Number {
         Number {
             value,
             on_line,
@@ -21,8 +26,12 @@ impl Number {
     }
 
     pub fn is_adjacent(&self, symb: &Symbol) -> bool {
-        if symb.on_line < self.on_line.saturating_sub(1) || symb.on_line > self.on_line + 1 {return false}
-        if symb.on_col < self.start_on_col.saturating_sub(1) || symb.on_col > self.end_on_col + 1 {return false}
+        if symb.on_line < self.on_line.saturating_sub(1) || symb.on_line > self.on_line + 1 {
+            return false;
+        }
+        if symb.on_col < self.start_on_col.saturating_sub(1) || symb.on_col > self.end_on_col + 1 {
+            return false;
+        }
         true
     }
 }
@@ -47,14 +56,20 @@ impl Symbol {
 pub(crate) fn collect_numbers(lines: &[String]) -> Vec<Number> {
     let reg_match_number: Regex = Regex::new(r"(\d+)").unwrap();
 
-    let matched_numbers: Vec<Number> = lines.iter()
+    let matched_numbers: Vec<Number> = lines
+        .iter()
         .enumerate()
         .flat_map(|(line_number, line)| {
-            reg_match_number.find_iter(line)
-                .map(move |m| Number::new(m.as_str().parse().unwrap(), 
-                                        line_number, 
-                                        m.start(), 
-                                        m.end()-1))
+            reg_match_number
+                .find_iter(line)
+                .map(move |m| {
+                    Number::new(
+                        m.as_str().parse().unwrap(),
+                        line_number,
+                        m.start(),
+                        m.end() - 1,
+                    )
+                })
                 .collect::<Vec<Number>>()
         })
         .collect();
@@ -64,13 +79,15 @@ pub(crate) fn collect_numbers(lines: &[String]) -> Vec<Number> {
 pub(crate) fn collect_symbols(lines: &[String]) -> Vec<Symbol> {
     let reg_match_symbol: Regex = Regex::new(r"([^0-9.])").unwrap();
 
-    let matched_symbols: Vec<Symbol> = lines.iter()
+    let matched_symbols: Vec<Symbol> = lines
+        .iter()
         .enumerate()
         .flat_map(|(line_number, line)| {
-            reg_match_symbol.find_iter(line)
-                .map(move |m| Symbol::new(m.as_str().chars().next().unwrap(),
-                        line_number,
-                            m.start()))
+            reg_match_symbol
+                .find_iter(line)
+                .map(move |m| {
+                    Symbol::new(m.as_str().chars().next().unwrap(), line_number, m.start())
+                })
                 .collect::<Vec<Symbol>>()
         })
         .collect();
@@ -80,7 +97,13 @@ pub(crate) fn collect_symbols(lines: &[String]) -> Vec<Symbol> {
 pub(crate) fn sum_adjacent(matched_numbers: &[Number], matched_symbols: &[Symbol]) -> usize {
     let sum = matched_numbers
         .iter()
-        .filter_map(|n| if matched_symbols.iter().any(|s| n.is_adjacent(s)) { Some(n.value) } else { None })
+        .filter_map(|n| {
+            if matched_symbols.iter().any(|s| n.is_adjacent(s)) {
+                Some(n.value)
+            } else {
+                None
+            }
+        })
         .sum::<usize>();
     sum
 }
@@ -96,19 +119,22 @@ pub(crate) fn sum_gears(matched_symbols: &[Symbol], matched_numbers: &[Number]) 
     let sum = star_symbols
         .iter()
         .filter_map(|star| {
-            let adj = matched_numbers.iter().filter(|m|m.is_adjacent(star)).cloned().collect::<Vec<Number>>();
-            if adj.len() == 2 {return Some(adj[0].value*adj[1].value)} 
+            let adj = matched_numbers
+                .iter()
+                .filter(|m| m.is_adjacent(star))
+                .cloned()
+                .collect::<Vec<Number>>();
+            if adj.len() == 2 {
+                return Some(adj[0].value * adj[1].value);
+            }
             None
         })
         .sum::<usize>();
     sum
-    }
+}
 
 pub(crate) fn read_lines(filename: &str) -> Vec<String> {
     let file = File::open(filename).unwrap();
     let reader = io::BufReader::new(file);
-    reader.lines()
-        .map_while(Result::ok)
-        .collect()
+    reader.lines().map_while(Result::ok).collect()
 }
-
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ const FILE_NAME: &str = "input.txt";
 //const FILE_NAME: &str = "testdata.txt";
 
 fn main() {
-    let lines: Vec<String> = read_lines(FILE_NAME); 
+    let lines: Vec<String> = read_lines(FILE_NAME);
     let matched_numbers: Vec<Number> = collect_numbers(&lines);
     let matched_symbols: Vec<Symbol> = collect_symbols(&lines);
     let sum: usize = sum_adjacent(&matched_numbers, &matched_symbols);
@@ -13,5 +13,3 @@ fn main() {
     let sum: usize = sum_gears(&matched_symbols, &matched_numbers);
     println!("Part 2 Sum:  {}", sum);
 }
-
-
